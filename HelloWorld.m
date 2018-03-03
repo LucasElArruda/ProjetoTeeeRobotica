d9fb54f0263df841d9a3c00b6318720a9e0ec466
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@ee.ufcg.edu.br>
2018-03-03 18:51:46 -0300

    Coloca o ePuck pra rodar.

--- HelloWorld.m
+++ HelloWorld.m
@@ -24,3 +24,26 @@ commThreadCycleInMs = 5;
 
 %% Conecta com o Vrep
 [clientID]=vrep.simxStart(connectionAddress,connectionPort,waitUntilConnected,doNotReconnectOnceDisconnected,timeOutInMs,commThreadCycleInMs);
+
+%handle pro motor esquerdo
+[returnCode,leftWheel]=vrep.simxGetObjectHandle(clientID,'ePuck_leftJoint',vrep.simx_opmode_blocking)
+[returnCode,rightWheel]=vrep.simxGetObjectHandle(clientID,'ePuck_rightJoint',vrep.simx_opmode_blocking)
+
+
+% Inicia simulação do vrep
+[returnCode] = vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot);
+
+%roda o boneco
+[returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,5,vrep.simx_opmode_blocking)
+[returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,0,vrep.simx_opmode_blocking)
+
+
+
+pause(10)
+
+vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait);
+
+
+vrep.simxFinish(clientID);
+
+vrep.delete();
