c2e6ae0f3dd97bcb2dc0a5c54be18e89dd56e9d1
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@ee.ufcg.edu.br>
2018-03-10 23:49:42 -0300

    Adiciona handlers para sensores de proximidade
    Se o sensor 4 detecta algo, o ePuck gira.

--- HelloWorld.m
+++ HelloWorld.m
@@ -37,7 +37,14 @@ end
 [returnCode,leftWheel]=vrep.simxGetObjectHandle(clientID,'ePuck_leftJoint',vrep.simx_opmode_blocking);
 [returnCode,rightWheel]=vrep.simxGetObjectHandle(clientID,'ePuck_rightJoint',vrep.simx_opmode_blocking);
 [returnCode,lightSensor]=vrep.simxGetObjectHandle(clientID,'ePuck_lightSensor',vrep.simx_opmode_blocking);
-
+[returnCode,proxSensor1]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor1',vrep.simx_opmode_blocking);
+[returnCode,proxSensor2]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor2',vrep.simx_opmode_blocking);
+[returnCode,proxSensor3]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor3',vrep.simx_opmode_blocking);
+[returnCode,proxSensor4]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor4',vrep.simx_opmode_blocking);
+[returnCode,proxSensor5]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor5',vrep.simx_opmode_blocking);
+[returnCode,proxSensor6]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor6',vrep.simx_opmode_blocking);
+[returnCode,proxSensor7]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor7',vrep.simx_opmode_blocking);
+[returnCode,proxSensor8]=vrep.simxGetObjectHandle(clientID,'ePuck_proxSensor8',vrep.simx_opmode_blocking);
 
 minlim = 1;
 maxlim = 16;
@@ -56,36 +63,31 @@ image = zeros(2,16);
 [returnCode,resolution,image]=vrep.simxGetVisionSensorImage2(clientID,lightSensor,1,vrep.simx_opmode_streaming)
 upperhalf = (resolution(1)/2 + 1):resolution(1);
 lowerhalf = 1:(resolution(1)/2);
-for i = 1:2000
+
+[returnCode,detectionState,detectedPoint,detectedObjectHandle,~]=vrep.simxReadProximitySensor(clientID,proxSensor4,vrep.simx_opmode_streaming)
+for i = 1:1000
     [returnCode,resolution,image]=vrep.simxGetVisionSensorImage2(clientID,lightSensor,1,vrep.simx_opmode_buffer);
-    if(~isempty(image))
+    [returnCode,detectionState,detectedPoint,detectedObjectHandle,~]=vrep.simxReadProximitySensor(clientID,proxSensor4,vrep.simx_opmode_buffer)
+    if(detectionState~= 0)
+        [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,minspeed/2,vrep.simx_opmode_oneshot);
+        [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,2*maxspeed,vrep.simx_opmode_oneshot);
+    elseif(~isempty(image))
         image(image>128) = 200;
         image(image <100) = 0;
-%         [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,0.5,vrep.simx_opmode_oneshot);
-%         [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,0.5,vrep.simx_opmode_oneshot);
-        %if(image(1,8) > image(2,8) || image(1,4) > image(2,4) || image(1,1) > image(2,1) || image(1,12) > image(2,12) || image(1,16) > image(2,16) )
-        %if(~isempty(find(image(1,minlim:maxlim) > image(2,minlim:maxlim))))
+
         if(~isempty(find(image(:,1:8)>image(:,  9:end))))
             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,maxspeed,vrep.simx_opmode_oneshot);
             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,minspeed,vrep.simx_opmode_oneshot);
         end
-        %if(image(1,8) < image(2,8) || image(1,4) < image(2,4) || image(1,1) < image(2,1) || image(1,12) < image(2,12) || image(1,16) < image(2,16) )
-        %if(~isempty(find(image(1,minlim:maxlim) < image(2,minlim:maxlim))))
         if(~isempty(find(image(:,1:8)<image(:,  9:end))))
             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,maxspeed,vrep.simx_opmode_oneshot);
             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,minspeed,vrep.simx_opmode_oneshot);
         end
-%         if(image(1,8) == image(2,8))
-%             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,0.5,vrep.simx_opmode_oneshot);
-%             [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,0.5,vrep.simx_opmode_oneshot);
-%         end
-        
+%     else
+%         [returnCode]=vrep.simxSetJointTargetVelocity(clientID,leftWheel,maxspeed,vrep.simx_opmode_oneshot);
+%         [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,minspeed,vrep.simx_opmode_oneshot);
     end
-%     image(i).setdatatype('uint8Ptr',1,resolution(1)*resolution(2)*1);
-%     if(image(i).Value(1) == 0)
-%         [returnCode]=vrep.simxSetJointTargetVelocity(clientID,rightWheel,0.5,vrep.simx_opmode_oneshot);
-%     end
-    %[returnCode,~,detectedPoint(i,1:3),detectedObjectHandle,~]=vrep.simxReadProximitySensor(clientID,lightSensor,vrep.simx_opmode_buffer);
+    
     pause(0.01 )
 end
 %pause(3)
